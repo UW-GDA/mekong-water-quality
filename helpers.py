89c6178cc53168de1b9609818ba904d4ea5cc834
Helen Miller <helen@ohana.org>
2024-01-29 17:51:54 -0800

    add file for helper functions

---
+++ helpers.py
@@ -0,0 +1,15 @@
+# %%
+
+import pystac_client
+import planetary_computer
+#import odc.stac
+import matplotlib.pyplot as plt
+from pystac.extensions.eo import EOExtension as eo
+
+# %%
+def get_catalog():
+    catalog = pystac_client.Client.open(
+        "https://planetarycomputer.microsoft.com/api/stac/v1",
+        modifier=planetary_computer.sign_inplace,
+    )
+    return catalog
